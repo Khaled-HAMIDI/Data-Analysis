3db9f28e9b12fa535427def300076fe06e0675a0
Khaled <fk_hamidi@esi.dz>
2019-12-18 13:08:19 +0100

    Finalizing the BikeShare project

--- Bike_share_proejct/Bikeshare.py
+++ Bike_share_proejct/Bikeshare.py
@@ -135,3 +135,109 @@ def station_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
+
+def trip_duration_stats(df):
+    """Displays statistics on the total and average trip duration."""
+
+    print('\nCalculating Trip Duration...\n')
+    start_time = time.time()
+
+    # display total travel time
+    print('The total travel time is : {} '.format(df['Trip Duration'].sum())) 
+
+    # display mean travel time
+    print('The mean travel time  is : {} '.format( df['Trip Duration'].mean()))
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
+
+def user_stats(df):
+    """Displays statistics on bikeshare users."""
+
+    #Verify if the datatframe conatins a collumn named 'Gender' and 'Birth Year' exp:washington
+    if ('Gender' and 'Birth Year') in df :
+        print('\nCalculating User Stats...\n')
+        start_time = time.time()
+
+        # Display counts of gender
+        print('Gender:')
+       
+        gender_1=df.groupby('Gender').count()['Trip Duration'].keys()[0]
+        count_gender_1=df.groupby('Gender').count()['Trip Duration'].iloc[0]
+        gender_2=df.groupby('Gender').count()['Trip Duration'].keys()[1]
+        count_gender_2=df.groupby('Gender').count()['Trip Duration'].iloc[1]
+       
+        print ('We have  {}  of  {}  and  {}  of  {}'.format( count_gender_1,gender_1,count_gender_2,gender_2))
+        
+        # Display counts of user types
+        print('\nUser types')
+
+        user_type_1=df.groupby('User Type').count()['Trip Duration'].keys()[0]
+        count_type_1=df.groupby('User Type').count()['Trip Duration'].iloc[0]
+        user_type_2=df.groupby('User Type').count()['Trip Duration'].keys()[1]
+        count_type_2=df.groupby('User Type').count()['Trip Duration'].iloc[1]
+
+        print ('We have {}  of  {} and   {}  of  {}'.format(count_type_1 ,user_type_1 ,count_type_2,user_type_2))
+
+
+        # Display earliest, most recent, and most common year of birth
+        print('\nThe earliest year of birth is {} '.format(df['Birth Year'].min()))
+        print('The most recent year of birth is {} '.format( df['Birth Year'].max()))
+        print('The most common year of birth is {} '.format( df['Birth Year'].mode().iloc[0]))
+
+
+        print("\nThis took %s seconds." % (time.time() - start_time))
+    else:
+        print('\nThere are only user types stats for this city')
+        start_time = time.time()
+       
+        print('\nUser types')
+        print ('We have {}  of  {} and   {}  of  {} '.format(  df.groupby('User Type').count()['Trip Duration'].iloc[0]  ,df.groupby('User Type').count()['Trip Duration'].keys()[0] ,df.groupby('User Type').count()['Trip Duration'].iloc[1],df.groupby('User Type').count()['Trip Duration'].keys()[1]))
+        print("\nThis took %s seconds." % (time.time() - start_time))
+
+    print('-'*40)
+
+
+def indiv_trip_data(df):
+    '''Display the individual trip data'''
+   
+    i=0
+    while (True):
+        display=input('\nWould you like to view individual trip data ? (type yes or no)  ')
+        if display.lower() != 'yes':
+            break
+        else:
+            for j in range(5):
+                print('-'*40)
+                print('\n This is the raw number {} : '.format(i+1))
+
+                #raw will conatain the pandas serie in json format 
+                raw=df.iloc[i].to_json()
+                raw_list=raw.split(',')
+
+                #Display the raw in the specified format
+                for item in raw_list:
+                    print('\n {}'.format(item))
+                i=i+1
+
+
+def main():
+    while True:
+        city, month, day = get_filters()
+        df = load_data(city, month, day)
+        
+        time_stats(df,day,month)
+        station_stats(df)
+        trip_duration_stats(df)
+        user_stats(df)
+        indiv_trip_data(df)
+
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
+
+
+if __name__ == "__main__":
+   
+    main()
