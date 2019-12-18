309675cccf0a9442669ddbdf4cb86e949c9164aa
Khaled <fk_hamidi@esi.dz>
2019-12-18 13:06:19 +0100

    Adding the stations and time stats to the BikeShare project

--- Bike_share_proejct/Bikeshare.py
+++ Bike_share_proejct/Bikeshare.py
@@ -82,3 +82,56 @@ def load_data(city, month, day):
         df = df[df['day'] ==int(day)]
 
     return df
+
+
+
+def time_stats(df,day,month):
+    """Displays statistics on the most frequent times of travel."""
+
+    print('\nCalculating The Most Frequent Times of Travel...\n')
+    start_time = time.time()
+
+    # display the most common month
+    if (month =='all'):
+        month_max=df['month'].value_counts().head(1)
+        print('the most common month is {} with count of  {} '.format( month_max.keys()[0] ,month_max.iloc[0]))
+
+
+    # display the most common day of week
+    if (day == 'all'):
+        day_max=df['day'].value_counts().head(1)
+        print('the most common day is day {} with count of  {} '.format( day_max.keys()[0] ,day_max.iloc[0]))
+
+    # display the most common start hour
+    hour_max=df['hour'].value_counts().head(1)
+    print("the most common start hour is {}  with the count of  {} ".format( hour_max.keys()[0] ,hour_max.iloc[0]))
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
+
+def station_stats(df):
+    """Displays statistics on the most popular stations and trip."""
+
+    print('\nCalculating The Most Popular Stations and Trip...\n')
+    start_time = time.time()
+
+    # display most commonly used start station
+    start_max=df['Start Station'].value_counts().head(1)
+    print('The most common start station is  {}  with count of  {} '.format( start_max.keys()[0] ,start_max.iloc[0] ))
+    
+    # display most commonly used end station
+    end_max=df['End Station'].value_counts().head(1)
+    print('The most common end station is {}  with count of  {} \n'.format( end_max.keys()[0] ,end_max.iloc[0] ))
+
+
+    # display most frequent combination of start station and end station trip
+    freq_combination=df.groupby(['Start Station','End Station']).count()['Trip Duration'][df.groupby(['Start Station','End Station']).count()['Trip Duration']==df.groupby(['Start Station','End Station']).count()['Trip Duration'].max()]
+    print('The most common combination of start and end station is  ')
+    print('     The start station : {}'.format(freq_combination.keys()[0][0]) )
+    print('     The end station  : {}'.format(freq_combination.keys()[0][1]) )
+    print('     With the count of : {}'.format(freq_combination.iloc[0])) 
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
